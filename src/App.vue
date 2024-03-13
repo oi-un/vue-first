db35e4140cd3e5c186064ee285d70c31a9b0516a
yunji_choi <choiyj@kakao.com>
2024-03-13 22:55:42 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,18 @@
 </script>
 
 <template>
-  
+  <h1>안녕 뷰 안녕 에반 유</h1>
+  <hr>
+  <h3>상품목록</h3>
+  <ul>
+    <li>아이폰</li>
+    <li>갤럭시</li>
+    <li>아이패드</li>
+    <li>갤럭시탭</li>
+  </ul>
+  <h3>Copyright 2024 Vue.js</h3>
 </template>
 
 <style scoped>
-
+  h1{color:skyblue}
 </style>
