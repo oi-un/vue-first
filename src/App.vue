bd592e705fbdeb4c53b92437371e96d87d66aaf1
yunji_choi <choiyj@kakao.com>
2024-03-13 23:18:44 +0900

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <script setup>
+import { ref } from 'vue';
 
+  const title = ref('상품목록..')
+  const products = ref(['아이폰','갤럭시','아이패드','갤럭시탭'])
+  const copyright = ref('Copyright 2024 Vue.js')
+  const imgSrc = 'https://w7.pngwing.com/pngs/854/555/png-transparent-vue-js-hd-logo-thumbnail.png'
+  const imgWidth = 100;
 </script>
 
 <template>
   <h1>안녕 뷰 안녕 에반 유</h1>
   <hr>
-  <h3>상품목록</h3>
+  <img :src="imgSrc" :width="imgWidth" />
+  <h3>{{title}}</h3>
   <ul>
-    <li>아이폰</li>
-    <li>갤럭시</li>
-    <li>아이패드</li>
-    <li>갤럭시탭</li>
+    <li>{{products[0]}}</li>
+    <li>{{products[1]}}</li>
+    <li>{{products[2]}}</li>
+    <li>{{products[3]}}</li>
   </ul>
-  <h3>Copyright 2024 Vue.js</h3>
+  <h3>{{copyright}}</h3>
 </template>
 
 <style scoped>
